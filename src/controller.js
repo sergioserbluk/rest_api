694de7429c16a27ee5a16487d1947cedacacf4ab
sergio Serbluk <sergiodserbluk@gmail.com>
2023-10-19 06:57:39 -0300

    Agrego las funcionalidades Create, update, delete

--- src/controller.js
+++ src/controller.js
@@ -9,7 +9,7 @@ async getOne(req, res){
     const [result]=await pool.query('SELECT * FROM libros WHERE id=?',[libro.id]);
     res.json(result);
 }
-/*
+
 async create(req, res){
     const libro=req.body;
     const [result]=await pool.query('INSERT INTO libros(nombre, autor, categoria, anio_publicacion, isbn) VALUES (?,?,?,?,?)',[libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.isbn]);
@@ -26,7 +26,7 @@ async delete(req, res){
     const [result]=await pool.query('DELETE FROM libros WHERE id=?',[libro.id]);
     res.json(result);
 }
-*/
+
 
 }
 export const libro = new LibrosController();
